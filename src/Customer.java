18b3526a1d2500f28808c60f580f761e0d1cdd50
Gundeep <Gundeep@DESKTOP-L10JNPV>
2019-08-01 18:00:06 -0400

    added sorting and display code

--- src/Customer.java
+++ src/Customer.java
@@ -1,28 +1,29 @@
+package src;
+
 import java.util.ArrayList;
 
 public class Customer implements IDisplay {
-	
-	
+
 	private int CustomerId;
 	private String firstName;
 	private String lastName;
-	private String fullName ;
+	private String fullName;
 	private String emailId;
 	private double totalAmount;
-	private ArrayList<Bill> billList; 
-	
-	public Customer(int CustomerId,String firstName,String lastName,String fullName,String emailId){
-		
+	private ArrayList<Bill> billList;
+
+	public Customer(int CustomerId, String firstName, String lastName, String fullName, String emailId) {
+
 		this.CustomerId = CustomerId;
-		this.firstName=firstName;
-		this.lastName=lastName;
-		this.fullName=fullName;
-		this.emailId=emailId;
-		this.billList=new ArrayList<Bill>();
-		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.fullName = fullName;
+		this.emailId = emailId;
+		this.billList = new ArrayList<Bill>();
+
 	}
-	
-	public void insertion(Bill Bill){
+
+	public void insertion(Bill Bill) {
 		this.billList.add(Bill);
 	}
 
@@ -82,39 +83,92 @@ public class Customer implements IDisplay {
 		this.billList = billList;
 	}
 
+	public double calcTotalAmount() {
+
+		double totamount = 0;
+		for (int i = 0; i < billList.size(); i++) {
+
+			totamount = totamount + billList.get(i).getBillAmount();
+
+		}
+
+		return totamount;
+
+	}
+
+	public void sortBills() {
+
+		int i, j, min_idx;
+
+		// One by one move boundary of unsorted subarray
+		for (i = 0; i < this.billList.size() - 1; i++) {
+			// Find the minimum element in unsorted array
+			min_idx = i;
+			for (j = i + 1; j < this.billList.size(); j++)
+				if (this.billList.get(j).getBillId() < this.billList.get(min_idx).getBillId()) {
+					min_idx = j;
+				}
+			// Swap the found minimum element with the first element
+			Bill billTemp = this.billList.get(i);
+			this.billList.set(i, this.billList.get(min_idx));
+			this.billList.set(min_idx, billTemp);
+
+		}
+
+	}
+
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
-		for(int i=0;i<billList.size();i++){
-			if(billList.get(i).getBillType()=="Mobile")
-			{
-				Mobile mobile=(Mobile) billList.get(i);
-				
-				String output="Bill Id : "+mobile.getBillId()+"\n"
-						+ "Bill Date : "+mobile.getDate()+"\n"
-						+ "Bill Type : "+mobile.getBillType()+"\n"
-						+ "Bill Amount : "+mobile.getBillAmount()+"\n"
-						+ "Manufacturer Name : "+ mobile.getManufactureName()+"\n"
-						+ "Plane Name : "+mobile.getPlanName()+"\n"
-						+ "Mobile Number : "+mobile.getMobileNumber()+"\n"
-						+ "Internet Usage : "+mobile.getDataUsed()+" GB\n"
-						+ "Minutes Usage : "+mobile.getMinuteUsed()+" minutes";
-						System.out.println(output);
-			
+
+		System.out.println("\n\nCustomer Id : " + this.getCustomerId() + "\nCustomer Full Name : " + this.getFullName()
+				+ "\nCustomer Email ID : " + this.getEmailId());
+
+		if (billList.size() > 0) {
+
+			System.out.println("\n---- Bill Information ----" + "\n******************************************");
+
+			this.sortBills();
+
+			for (int i = 0; i < billList.size(); i++) {
+				if (billList.get(i).getBillType() == "Mobile") {
+					Mobile mobile = (Mobile) billList.get(i);
+
+					String output1 = "Bill Id : " + mobile.getBillId() + "\n" + "Bill Date : " + mobile.getDate() + "\n"
+							+ "Bill Type : " + mobile.getBillType() + "\n" + "Bill Amount : " + mobile.getBillAmount()
+							+ "\n" + "Manufacturer Name : " + mobile.getManufactureName() + "\n" + "Plane Name : "
+							+ mobile.getPlanName() + "\n" + "Mobile Number : " + mobile.getMobileNumber() + "\n"
+							+ "Internet Usage : " + mobile.getDataUsed() + " GB\n" + "Minutes Usage : "
+							+ mobile.getMinuteUsed() + " minutes";
+					System.out.println(output1);
+
+				} else if (billList.get(i).getBillType() == "Hydro") {
+					Hydro hydro = (Hydro) billList.get(i);
+					String output2 = "Bill Id : " + hydro.getBillId() + "\nBill Date : " + hydro.getDate()
+							+ "\nBill Type : " + hydro.getBillType() + "\nBill Amount : " + hydro.getBillAmount()
+							+ "\nAgency Name : " + hydro.getAgencyName() + "\nUnit Consumed :" + hydro.getUnitConsume()
+							+ "units";
+					System.out.println(output2);
+
+				} else if (billList.get(i).getBillType() == "Internet") {
+
+					Internet internet = (Internet) billList.get(i);
+
+					String output3 = "\nBill Id : " + internet.getBillId() + "\nBill Date : " + internet.getDate()
+							+ "\nBill Type : " + internet.getBillType() + "\nBill Amount : " + internet.getBillAmount()
+							+ "\nProvider Name : " + internet.getProviderName() + "\nInternet Usage :"
+							+ internet.getUsage() + "GB";
+					System.out.println(output3);
+				}
+				System.out.println("******************************************");
 			}
-			else if(billList.get(i).getBillType()=="Hydro")
-			{
-				Hydro hydro=(Hydro) billList.get(i);
-				String output2="Bill Id : "+ hydro.getBillId()+"\n"
-						+ "Bill Date : "+ hydro.getDate()+"\n"
-						+ "Bill Type : "+ hydro.getBillType()+"\n"
-						+ "Bill Amount : "+ hydro.getBillAmount();
-				
-				
-				
-			}
-			
+			System.out.println("******************************************");
+			System.out.println("Total Bill Amount to Pay : $" + calcTotalAmount());
+			System.out.println("******************************************");
+		} else {
+			System.out.println("~~~NOTE : This Customer has no bills");
 		}
+
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+package src;
+
+
 import java.util.ArrayList;
 
 public class Main {
@@ -5,11 +8,54 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Customer objcustomer =new Customer(1,"Harman","Singh","Harman Singh","harman@gmail.com");
-		ArrayList<Customer> objcustomer1=new ArrayList<Customer>();
-		objcustomer1.add(objcustomer);
-		objcustomer.insertion(new Mobile(1,"31/07/19","Mobile", 116, "Iphone XS", "Prepaid", "6476670240", 7, 250));
-		objcustomer.display();
+		ArrayList<Customer> custlist=new ArrayList<Customer>();
+		Customer objcustomer1 =new Customer(10,"Harman","Singh","Harman Singh","harman@gmail.com");
+		objcustomer1.insertion(new Mobile(3,"31/07/19","Mobile", 116, "Iphone XS", "Prepaid", "6476670240", 7, 250));
+		objcustomer1.insertion(new Mobile(2,"31/07/19","Mobile", 116, "Iphone XS", "Prepaid", "6476670240", 7, 250));
+		objcustomer1.insertion(new Mobile(1,"31/07/19","Mobile", 116, "Iphone XS", "Prepaid", "6476670240", 7, 250));
+		
+		Customer objcustomer2 =new Customer(2,"Harman","Singh","Harman Singh","harman@gmail.com");
+		
+		
+		
+		
+		
+		custlist.add(objcustomer1);
+		custlist.add(objcustomer2);
+		
+		sortCustomers(custlist);
+		
+		
+		
+		for(int i=0; i < custlist.size() ; i++) {
+			custlist.get(i).display();
+		}
+		
+	}
+	
+
+	public static ArrayList<Customer> sortCustomers(ArrayList<Customer> custlist) {		
+			
+			 int i, j, min_idx;  
+			  
+			    // One by one move boundary of unsorted subarray  
+			    for (i = 0; i < custlist.size()-1; i++)  
+			    {  
+			        // Find the minimum element in unsorted array  
+			        min_idx = i;  
+			        for (j = i+1; j < custlist.size(); j++)  
+			        if (custlist.get(j).getCustomerId() < custlist.get(min_idx).getCustomerId()) { 
+			            min_idx = j;  
+			        }
+			        // Swap the found minimum element with the first element  
+			        Customer custtemp = custlist.get(i);
+			        custlist.set(i, custlist.get(min_idx));
+			        custlist.set(min_idx, custtemp);
+			      
+				
+			}
+			    return custlist;
+		
 		
 	}
 
